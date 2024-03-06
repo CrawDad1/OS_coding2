5808b06c17b25af8b83cfb165c19b72c776afabd
CrawDad1 <dcrawford1992@live.com>
2024-03-06 01:10:17 -0600

    Implemented Scheduler, Process, and main

--- README.txt
+++ README.txt
@@ -1,3 +1,12 @@
 # Operating Systems : Coding Assignment 2
 
 This is a coding assignment for the Operating Systems course at WSU
+
+This code defines both a Process and Scheduler class, each with a static member to keep track of unique ID's within each class type. 
+
+Each Scheduler maintains a list of pointers to Process type objects. 
+
+when creating a process, it is given 5 tickets in sequence based on the Scheduler::next_ticket variable. then, a pointer to this new process is added to Scheduler::p_list.
+
+when drawing lottery tickets, a Scheduler checks the tickets held by each process in that scheduler's p_list. When a match is found, Process.Run() is called for the matching process. 
+
---
+++ main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+class Process {
+  private: 
+  static int next_process;
+  int process_id = 0;
+  
+  public:
+  vector<int> tickets = vector<int>(0);
+  
+
+  Process():tickets({}){
+    this->process_id = Process::next_process;
+    Process::next_process++;
+  }
+
+  Process(const vector<int>& new_tickets):Process(){
+    this->tickets = new_tickets;
+  }
+
+  int get_pid(){return this->process_id;}
+
+  void run(){
+    cout << "Process " << this->process_id << " has ran!" << endl;
+  }
+};
+int Process::next_process = 0;
+
+
+class Scheduler {
+private:
+  static int next_ticket;
+  
+public:
+  vector<Process*> p_list = {};
+  
+  // Constructor
+  Scheduler():p_list({}){}
+
+  ~Scheduler(){
+    for (auto p : this->p_list) {
+      delete p;
+    }
+  }
+
+  // functions
+  void add_tickets(Process& p){
+    for(int i = 0; i < 5; i++){
+      p.tickets.push_back(Scheduler::next_ticket);
+      Scheduler::next_ticket++;
+    }
+  }
+
+  void add_process(){
+    Process* p = new Process();
+    add_tickets(*p);
+    this->p_list.push_back(p);
+  }
+
+  void draw_ticket(){
+    // generate a number between 0 and next_ticket - 1
+    int num = (rand() % Scheduler::next_ticket);
+
+    cout << "Scheduler drawing lottery ticket: " << num << endl;
+
+    // check who has the ticket
+    for(auto& p : this->p_list){
+      for(int tic : p->tickets){
+        if (tic == num) {
+          p->run();
+          break;
+        }
+      }
+    }
+  }
+  
+  
+  
+};
+int Scheduler::next_ticket = 0;
+
+int main(){
+  srand(time(nullptr));
+  cout << "Hello world!" << endl;
+
+  cout << "\n\nCreating Scheduler... ";
+  Scheduler s = Scheduler();
+  cout << "Done!" << endl;
+
+
+  cout << "\n\nCreating 5 Processes... ";
+  s.add_process();
+  s.add_process();
+  s.add_process();
+  s.add_process();
+  s.add_process();
+  cout << "Done!" << endl;
+
+
+  cout << "\n\nDrawing 5 Tickets... " << endl;
+
+  s.draw_ticket();
+  s.draw_ticket();
+  s.draw_ticket();
+  s.draw_ticket();
+  s.draw_ticket();
+
+  cout << "DONE!" << endl;
+
+  
+  return 0;
+}
